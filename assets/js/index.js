ac6954b2725c7e4e5c72401c2455b165ab14bb10
eternal198 <ikuya313198@gmail.com>
2021-04-11 20:26:08 +0900

    fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
 // hamburgerMenu_back
-(() => {
-  const back = document.getElementById("js-hamburgerMenu_back");
-  const menu = document.getElementById("js-hamburgerMenu");
-  back.addEventListener('click', () => {
-    menu.classList.toggle("u-hidden");
-  });
-})();
+const back = document.getElementById("js-hamburgerMenu_back");
+const menu = document.getElementById("js-hamburgerMenu");
+const button = document.getElementById("js-hamburgerMenu_button");
+
+const toggleMenu = () => {
+  menu.classList.toggle("u-hidden");
+  back.classList.toggle("u-hidden");
+}
+
+back.addEventListener('click', () => {
+  toggleMenu();
+});
+button.addEventListener('click', () => {
+  toggleMenu();
+});
 
 
 // scrollReveal
